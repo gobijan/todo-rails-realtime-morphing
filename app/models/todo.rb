1e1a34821086f3d771e257feb124082bd1b54834
Bijan Rahnema <bijan@screenisland.com>
2023-12-15 20:02:08 +0100

    make it work with main branch

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,6 @@ class Todo < ApplicationRecord
   validates :title, presence: true
   validates :completed, inclusion: {in: [true, false]}
 
-  broadcasts_refreshes
+  # Hack. See: https://github.com/hotwired/turbo-rails/issues/545
+  broadcasts_refreshes_to ->(stream) { stream.class.broadcast_target_default }
 end
-
